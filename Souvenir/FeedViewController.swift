2e23bd74bc25828eb448d60de38bf68d33b8e4db
Amber Spadafora <ahelenespadafora@gmail.com>
2019-05-17 19:27:14 -0400

    Add side menu bar button item

---
+++ Souvenir/FeedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FeedViewController.swift
+//  souvenir
+//
+//  Created by Amber Spadafora on 5/17/19.
+//  Copyright © 2019 Amber Spadafora. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
